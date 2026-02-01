ba5b2d42a97379d888e9a5fb8ba4a7555ac92f53
Harry-sai <hariom_k@bt.iitr.ac.in>
2026-02-01 21:48:45 +0530

    Initial commit: Digital Twin infrastructure and application

---
+++ terraform/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Main region (India)
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Required ONLY for CloudFront / ACM
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
